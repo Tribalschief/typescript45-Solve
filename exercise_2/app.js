388e6d64cd5b765bdab8a6a0941c89dc93fd6982
architectureYK <mahmedyk789@gmail.com>
2024-02-23 21:16:01 +0500

    modified exercise 2

---
+++ exercise_2/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//task3
+let person_name2 = "ahmed Yar Khan";
+console.log(person_name2.toLowerCase());
+console.log(person_name2.toUpperCase());
+console.log(console.log(person_name2.charAt(0).toUpperCase() + person_name2.slice(1).toLowerCase()));
--- exercise_2/app.ts
+++ exercise_2/app.ts
@@ -1,5 +1,5 @@
 //task3
-let person_name2:string = "Ahmed yar khan"
+let person_name2:string = "ahmed Yar Khan"
 console.log(person_name2.toLowerCase())
 console.log(person_name2.toUpperCase())
-console.log(person_name2.charAt(0).toUpperCase())
+console.log(console.log(person_name2.charAt(0).toUpperCase() + person_name2.slice(1).toLowerCase()));
